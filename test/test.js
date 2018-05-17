fda1aec251f3fcf89477ab5d0167a2a532477c13
recep_hidir <recep.hidir@gmail.com>
2018-05-17 11:12:26 +0300

    jest callback example added.

--- test/test.js
+++ test/test.js
@@ -6,8 +6,6 @@ function fetchData(callbackFunction){
     callbackFunction(foo);
 }
 
-
-
 describe('array example', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
