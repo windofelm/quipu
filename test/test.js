d0a6ee9ef84c88a2b1ead031a9e8a53143f60c83
recep_hidir <recep.hidir@gmail.com>
2018-05-17 11:11:26 +0300

    jest callback example added.

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ function fetchData(callbackFunction){
 
 
 
-describe('Array', function() {
+describe('array example', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal([1,2,3].indexOf(4), -1);
@@ -29,31 +29,34 @@ describe('mapArrow', function() {
 });
 
 // Jest
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
-});
+describe('jest examples', function() {
+    test('two plus two is four', () => {
+        expect(2 + 2).toBe(4);
+    });
 
-test('object assignment', () => {
-    const data = {one: 1};
-    data['two'] = 2;
-    expect(data).toEqual({one: 1, two: 2});
-});
+    test('object assignment', () => {
+        const data = {one: 1};
+        data['two'] = 2;
+        expect(data).toEqual({one: 1, two: 2});
+    });
 
-test('adding positive numbers is not zero', () => {
-    for (let a = 1; a < 10; a++) {
-        for (let b = 1; b < 10; b++) {
-            expect(a + b).not.toBe(0);
+    test('adding positive numbers is not zero', () => {
+        for (let a = 1; a < 10; a++) {
+            for (let b = 1; b < 10; b++) {
+                expect(a + b).not.toBe(0);
+            }
         }
-    }
+    });
+
+    test('the data is peanut butter', done => {
+        function callback(data) {
+            expect(data).toBe('peanut butter');
+            done();
+        }
+
+        fetchData(callback);
+    });
 });
 
-test('the data is peanut butter', done => {
-    function callback(data) {
-        expect(data).toBe('peanut butter');
-        done();
-    }
-
-    fetchData(callback);
-});
 
 // test finish.
